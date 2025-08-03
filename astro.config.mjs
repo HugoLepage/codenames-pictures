b5e2d6647fc356a0f06c6b437e40f9cc4ddb8486
Hugo Lepage <hugo.lepage@gmail.com>
2025-08-03 15:51:13 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://hugolepage.github.io/codenames-pictures',
+  base: '/codenames-pictures/', // Base path for GitHub Pages
+  output: 'static', // Still use static output for GitHub Pages
+});
